3ded7a454021372ce40a9c8b759ff40b57aae405
JiongCHEN <gakki91@gmail.com>
2016-06-17 15:35:35 +0800

    add maxima mode

--- init.el
+++ init.el
@@ -105,3 +105,13 @@
 (add-to-list 'load-path "/home/jerry/.emacs.d/elpa/rainbow-delimiters-2.1.3")
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+
+;; Maxima
+(add-to-list 'load-path "/usr/share/maxima/5.32.1/emacs/")
+(autoload 'maxima-mode "maxima" "Maxima mode" t)
+(autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
+(autoload 'maxima "maxima" "Maxima interaction" t)
+(autoload 'imath-mode "imath" "Imath mode for math formula input" t)
+(setq imaxima-use-maxima-mode-flag t)
+(add-to-list 'auto-mode-alist '("\\.ma[cx]" . maxima-mode))
